67cc4b2694285cc09aa9d5a6630fb299bedca30d
yuusuf <yuusuf98@outlook.com>
2026-05-26 13:06:13 +0200

    fetch weather api and use the data.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
-export default function Home() {
+
+
+export default async function Home() {
+const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q=jeddah&appid=1f17f8a5fc6834eb37400bad9da828cd&units=metric');
+const data = await res.json()
+
+console.log(data)
+
+
+
+
   return (
     <div className="container">
       <div className="card">
         <input type="text" placeholder="search..."/>
-        <p>☀️</p>
-        <p>24°C</p>
-        <p>Aarhus</p>
-        <p>68% Humidity</p>
-        <p>5.67 km/h Wind Speed</p>
+        <img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}/>
+        <p>{data.main.temp} °C</p>
+        <p>{data.name}</p>
+        <p>{data.main.humidity} Humidity</p>
+        <p>{data.wind.speed} km/h Wind Speed</p>
       </div>
     </div>
     
